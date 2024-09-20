2b2255abccdb1c9a7212ecd998a75a26ad9ebad5
Gersten <gervins@web.de>
2024-09-20 23:34:56 +0700

    html code, website added

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3002;
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World Bye !!!');
+// });
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes
 app.get('/', (req, res) => {
-  res.send('Hello World Bye !!!');
+  res.sendFile(path.join(__dirname, 'public_html', 'index.html'));
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
