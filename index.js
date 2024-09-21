a63960750280d9b7e902f489908df389d61117ed
Gersten <gervins@web.de>
2024-09-21 22:26:35 +0700

    forgot FS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 // const http = require('http');
+const fs = require('fs');
 const WebSocket = require('ws');
 
 // const helper = require('./helper');  // Import helper.js
